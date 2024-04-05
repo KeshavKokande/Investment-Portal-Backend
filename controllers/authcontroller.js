a5bc6c903005d8d6b8594eba462c66bfba26652c
Keshav Kokande <keshavkesu05@gmail.com>
2024-04-05 17:58:47 +0530

    Revert "sameSite:None"
    This reverts commit 5ecc17319b8f99f8d7a81a4bf3e48d3fbee0e2da.

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -27,9 +27,7 @@ const createSendToken = (user, statusCode, res) => {
 
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: false,
-        sameSite: 'None', // Set SameSite attribute to None for cross-site usage
-        secure: true // Require secure connection for SameSite=None
+        httpOnly: false
     };
 
     // if(process.env.NODE_ENV === 'production'x){
