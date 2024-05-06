58f6aeb125297d75be61ec040f4f64a9e1d36edb
Keshav Kokande <keshavkesu05@gmail.com>
2024-05-07 02:56:05 +0530

    added SameSite

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -29,7 +29,8 @@ const createSendToken = (user, statusCode, res) => {
 
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: true,
+        sameSite: 'None'
     };
 
     // if(process.env.NODE_ENV === 'production'x){
@@ -309,7 +310,8 @@ exports.OauthJWTtoken = asyncErrorHandler(async(req, res, next) => {
  
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: true,
+        sameSite: 'None'
     };
  
     res.cookie('jwt', token, cookieOptions);
