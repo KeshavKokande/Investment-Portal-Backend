fe95874b45c0490ea46999cbaa441dc9810e9f7c
Shubham Goswami <shubham24976@gmail.com>
2024-05-07 11:52:02 +0530

    httpsOnly removed

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -28,8 +28,8 @@ const createSendToken = (user, statusCode, res) => {
     const token = signToken(user.email);
 
     const cookieOptions = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000)
+        //httpsOnly: true
     };
 
     // if(process.env.NODE_ENV === 'production'x){
