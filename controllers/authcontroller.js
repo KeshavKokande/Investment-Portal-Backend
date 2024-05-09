73e7261d5a4e56fd7e736bc4a68cce146ff465d0
manran03 <92865525+manran03@users.noreply.github.com>
2024-05-09 18:45:35 +0530

    Update authcontroller.js

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -312,7 +312,7 @@ exports.OauthJWTtoken = asyncErrorHandler(async(req, res, next) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: 'None',
+        sameSite: 'Lax',
         secure: true,
         domain: '.azurewebsites.net'
     };
