35af9f0368c77a7707e15948d26916e6aa59200e
Keshav Kokande <keshavkesu05@gmail.com>
2024-05-09 16:10:11 +0530

    samesite:lax

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -30,7 +30,7 @@ const createSendToken = (user, statusCode, res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: 'None',
+        sameSite: 'Lax',
         secure: true
     };
 
