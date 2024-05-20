16e9f6dc22ab931b3b7717b2a10edfbbdf4fa603
Shubham Goswami <shubham24976@gmail.com>
2024-05-20 11:53:49 +0530

    Removed name of plan from gen AI

--- utils/getPlanDescrpGenAI.js
+++ utils/getPlanDescrpGenAI.js
@@ -6,7 +6,7 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 async function getPlanDescription(stocks) {
     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-    const prompt = `Write a description and a name of a investment plan which consists of stocks: ${stocks}. Do not disclose the stock names and leverage the returns on these stocks in the last 2 years. Keep the description within 5 lines and appealing to the new plan buyers.`;
+    const prompt = `Write a description of a investment plan which consists of stocks: ${stocks}. Do not disclose the stock names and leverage the returns on these stocks in the last 2 years. Keep the description within 5 lines and appealing to the new plan buyers.`;
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
