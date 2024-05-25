57f4fe923b10281743bc762907192dca1edd5bf6
mukulxpatwal <mukulxpatwal@gmail.com>
2024-05-25 22:48:25 +0530

    sso

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -718,7 +718,7 @@ exports.resetPassword = asyncErrorHandler(async(req, res, next) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/v1/check-auth/auth/google/callback",
+    callbackURL: "hhtps://invest-public.azurewebsites.net/api/v1/check-auth/auth/google/callback",
     passReqToCallback: true
     },
     async function (request, accessToken, refreshToken, profile, done)  {
